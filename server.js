1d11bb592fba64e0a16c6729a0e7af250180c881
TTKHScriptPoetry <kimdotnetpossible@att.net>
2022-03-31 23:47:43 -0500

    applied env app key value

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 const sess = {
-  secret: 'Super secret',
+  secret: process.env.APP_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
